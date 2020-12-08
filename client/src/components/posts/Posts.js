185c28e72df71c79ec1833876d461231bc332563
vasurajan <63305945+vasurajan@users.noreply.github.com>
2020-12-09 02:13:09 +0530

    updated

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import Spinner from "../layout/Spinner";
 import PostItem from "./PostItem";
 import PostForm from "./PostForm";
 import { getPosts } from "../../actions/post";
@@ -10,12 +11,13 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
     getPosts();
   }, [getPosts]);
 
-  return (
-    <Fragment>
-      <section className="dash">
+  return loading ? (
+    <Spinner />
+  ) : (
+      <Fragment>
         <h1 className="large text-primary">Posts</h1>
         <p className="lead">
-          <i className="fas fa-user-secret" /> Welcome to the community
+          <i className="fas fa-user" /> Welcome to the community
       </p>
         <PostForm />
         <div className="posts">
@@ -23,9 +25,8 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
             <PostItem key={post._id} post={post} />
           ))}
         </div>
-      </section>
-    </Fragment>
-  );
+      </Fragment>
+    );
 };
 
 Posts.propTypes = {
